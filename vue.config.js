00d0256a9d20d5d24c2b55a962bc8ad73473af01
dolsing <dolsing@163.com>
2022-05-12 20:19:23 +0800

    Enter

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath:'./',
+  publicPath:'./',  //不添加这句话本地就无法运行
   transpileDependencies: true,
   lintOnSave:false
 })
